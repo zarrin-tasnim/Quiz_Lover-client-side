8f18ce1a5c0b42e86146a064f67b2e526772f7ba
zarrin-tasnim <zarrintasnim200420@gmail.com>
2022-10-11 21:34:42 +0600

    title and favicon changed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import logo from './logo.svg';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import Main from './layout/Main';
+import Home from './components/Home/Home';
+import Statistics from './components/Statistics/Statistics';
+
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Main></Main>,
+      children: [
+        {
+          path: '/',
+          loader: () => fetch('t-shirts.json'),
+          element: <Home></Home>
+        },
+        {
+          path: '/order',
+          element: <Statistics></Statistics>
+        }
+      ]
+    }
+  ])
   return (
     <div className="App">
-
+      <RouterProvider router={router} ></RouterProvider>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from './reportWebVitals'; 
+
+// bootstrap css
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
