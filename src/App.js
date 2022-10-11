55b9802ff2290dda50864db40c33b27a13f2d57f
zarrin-tasnim <zarrintasnim200420@gmail.com>
2022-10-12 05:23:00 +0600

    statistics added

--- src/App.js
+++ src/App.js
@@ -30,6 +30,9 @@ function App() {
         },
         {
           path: '/statistics',
+          loader: async () => {
+            return fetch('https://openapi.programming-hero.com/api/quiz');
+          },
           element: <Statistics></Statistics>
         },
         {
--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,9 +1,47 @@
 import React from 'react';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { Bar, BarChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
+import './Statistics.css';
+import { useLoaderData } from 'react-router-dom';
 
 const Statistics = () => {
+    // const data = useLoaderData();
+    const [total, setTotal] = useState([])
+    useEffect(() => {
+        fetch('https://openapi.programming-hero.com/api/quiz')
+            .then(res => res.json())
+            .then(data => {
+                const totalLoaded = data.data;
+                const totalData = totalLoaded.map(card => {
+                    const singleCard = {
+                        name: card.name,
+                        total: card.total
+                    }
+                    return singleCard
+                })
+                console.log(totalData)
+                setTotal(totalData);
+            })
+    }, [])
+
     return (
         <div>
-            
+            <div className="container mt-5">
+                <div className="row">
+                    <div className="col  me-auto">
+                        <ResponsiveContainer width="100%" height={400}>
+                            <BarChart width={300} height={400} data={total}>
+                                <Bar dataKey="total" fill=" black" />
+                                <XAxis dataKey="name"></XAxis>
+                                <YAxis dataKey="total"></YAxis>
+                                <Tooltip></Tooltip>
+                            </BarChart>
+                        </ResponsiveContainer>
+                    </div>
+                </div>
+            </div>
+
         </div>
     );
 };
